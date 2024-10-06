07e4f4dcf4536e79cc66424da35d0a5aa6edc83e
Angel <angelclaudio@outlook.com>
2024-10-06 15:14:16 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Angel Claudio on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
